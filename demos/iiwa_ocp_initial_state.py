26ec065559e51ea762edb6a5dc4c0fad62f25802
Sebastien Kleff <skleff@akanagi.laas.fr>
2021-09-18 17:33:22 +0200

    ik sampling ok

--- demos/iiwa_ocp_initial_state.py
+++ demos/iiwa_ocp_initial_state.py
@@ -26,6 +26,9 @@ np.set_printoptions(precision=4, linewidth=180)
 # # # # # # # # # # # # 
 # Read config file
 config = path_utils.load_config_file('static_reaching_task_ocp')
+q0 = np.asarray(config['q0'])
+v0 = np.asarray(config['dq0'])
+x0 = np.concatenate([q0, v0])   
 # Get pin wrapper
 robot = IiwaConfig.buildRobotWrapper()
 # Get initial frame placement + dimensions of joint space
@@ -34,15 +37,9 @@ nq, nv = robot.model.nq, robot.model.nv
 nx = nq+nv
 nu = nq
 INIT_STATES = []
-N_STATES = 20
+N_STATES = 100
 np.random.seed(1)
 state = crocoddyl.StateMultibody(robot.model)
-# np.random.seed(1)
-# low = [-2.9671, -2.0944 ,-2.9671 ,-2.0944 ,-2.9671, -2.0944, -3.0543]
-# high = [2.9671, 2.0944, 2.9671, 2.0944, 2.9671, 2.0944, 3.0543]
-# for i in range(10):
-#     q0 = np.random.uniform(low=low, high=high, size=(nq,))
-#     INIT_STATES.append(np.concatenate([q0, np.zeros(nv)]))
 
 # Sampling conservative range for the state : 95% q limits and [-0.5, +0.5] v limits
 def get_samples(nb_samples:int):
@@ -59,8 +56,39 @@ def get_samples(nb_samples:int):
         samples.append( np.random.uniform(low=-x_max, high=+x_max, size=(nx,)))
     return samples
 
+import pinocchio as pin
+def get_samples_IK(nb_samples:int, q0, p_des, v_des, id_endeff, eps_p=0.1, eps_v=0.1):
+    # Sample several states 
+    N_SAMPLES = nb_samples
+    TSK_SPACE_SAMPLES = []
+    JNT_SPACE_SAMPLES = []
+    # Define bounds in cartesian space to sample (p_EE,v_EE) around (p_des,0)
+    p_min = p_des - eps_p; p_max = p_des + eps_p
+    v_min = v_des - eps_v; v_max = v_des + eps_v
+    y_min = np.concatenate([p_min, v_min])
+    y_max = np.concatenate([p_max, v_max])
+    # Generate samples (uniform)
+    for i in range(N_SAMPLES):
+        # Sample
+        y_EE = np.random.uniform(low=y_min, high=y_max, size=(6,))
+        TSK_SPACE_SAMPLES.append( y_EE )
+        # print(" Task sample  = ", y_EE)
+        # IK
+        q, _, _ = pin_utils.IK_position(robot, q0, id_endeff, y_EE[:3],
+                                        DISPLAY=False, LOGS=False, DT=1e-1, IT_MAX=1000, EPS=1e-6)
+        pin.computeJointJacobians(robot.model, robot.data, q)
+        robot.framesForwardKinematics(q)
+        J_q = pin.getFrameJacobian(robot.model, robot.data, id_endeff, pin.ReferenceFrame.LOCAL) 
+        vq = np.linalg.pinv(J_q)[:,:3].dot(y_EE[3:]) 
+        x = np.concatenate([q, vq])
+        JNT_SPACE_SAMPLES.append( x )
+    return JNT_SPACE_SAMPLES
 
-INIT_STATES = get_samples(N_STATES)
+
+# INIT_STATES = get_samples(N_STATES)
+p_des = np.asarray(config['p_des'])
+v_des = np.zeros(3)    
+INIT_STATES = get_samples_IK(N_STATES, q0, p_des, v_des, id_endeff, eps_p=0.05, eps_v=0.01)
 
 #################
 ### OCP SETUP ###
@@ -114,6 +142,6 @@ val_max = np.max(COSTS)
 index_max = COSTS.index(val_max)
 print("MAX COST = ", val_max, " at index ", index_max)
 
-fig, ax = plot_utils.plot_ddp_results(DDP_DATA, which_plots=['x','u','p'], SHOW=True, sampling_plot=1)
-# if(rejected!=0):
-#     fig, ax = plot_utils.plot_ddp_results(REJECTED_DATA, which_plots=['x','u','p'], SHOW=True, sampling_plot=10)
+fig, ax = plot_utils.plot_ddp_results(DDP_DATA, which_plots=['x','u','p'], SHOW=True, sampling_plot=2)
+if(rejected!=0):
+    fig, ax = plot_utils.plot_ddp_results(REJECTED_DATA, which_plots=['x','u','p'], SHOW=True, sampling_plot=10)
