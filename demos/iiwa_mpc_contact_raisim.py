fixed bug in measured forces computation

demos/iiwa_mpc_contact_raisim.py
@@ -61,7 +61,7 @@ id_endeff = robot.model.getFrameId('contact')
 M_ct              = robot.data.oMf[id_endeff].copy() 
   # Initial placement of contacted object in simulator
 contact_placement = robot.data.oMf[id_endeff].copy()
-offset = 0.025 #0.025 #0.045 + 0.08 - 0.1
+offset = 0.035 #0.025 #0.045 + 0.08 - 0.1
 contact_placement.translation = contact_placement.act(np.array([0., 0., offset])) 
 env.display_contact_surface(contact_placement, radius=0.1) 
 print("-----------------------")
@@ -79,14 +79,12 @@ import pinocchio as pin
 f_ext = []
 for i in range(nq+1):
     # CONTACT --> WORLD
-    W_X_ct = contact_placement.action
+    W_M_ct = contact_placement.copy()
+    f_WORLD = W_M_ct.act(pin.Force(np.asarray(config['f_des'])))
     # WORLD --> JOINT
-    j_X_W  = robot.data.oMi[i].actionInverse
-    # CONTACT --> JOINT
-    j_X_ee = W_X_ct.dot(j_X_W)
-    # ADJOINT INVERSE (wrenches)
-    f_joint = j_X_ee.T.dot(np.asarray(config['f_des']))
-    f_ext.append(pin.Force(f_joint))
+    j_M_W = robot.data.oMi[i].copy().inverse()
+    f_JOINT = j_M_W.act(f_WORLD)
+    f_ext.append(f_JOINT)
 # print(f_ext)
 u0 = pin_utils.get_tau(q0, v0, np.zeros((nq,1)), f_ext, robot.model)
 
@@ -101,17 +99,16 @@ ddp = ocp_utils.init_DDP(robot, config, x0, callbacks=False,
                                             u_reg_ref=u0) 
 SOLVE_AND_PLOT_INIT = True
 
+
 xs_init = [x0 for i in range(N_h+1)]
 us_init = [u0 for i in range(N_h)]
 
 if(SOLVE_AND_PLOT_INIT):
-  ddp.solve([x0 for i in range(N_h+1)], [u0 for i in range(N_h)], maxiter=100, isFeasible=False)
+  ddp.solve(xs_init, us_init, maxiter=100, isFeasible=False)
   # ddp_data = data_utils.extract_ddp_data(ddp)
   # fig, ax = plot_utils.plot_ddp_results(ddp_data, markers=['.'], which_plots=['x','u','p', 'f'], SHOW=True)
-
-
-        for k,m in enumerate(ddp.problem.runningModels):
-          m.differential.
+        # for k,m in enumerate(ddp.problem.runningModels):
+        #   m.differential.
 
 # # # # # # # # # # #
 ### INIT MPC SIMU ###
@@ -193,8 +190,8 @@ for i in range(sim_data['N_simu']):
     if(i%int(freq_SIMU/freq_PLAN) == 0):
         # print("PLAN ("+str(nb_plan)+"/"+str(sim_data['N_plan'])+")")
         # Update OCP 
-        for k,m in enumerate(ddp.problem.runningModels):
-          m.differential.
+        # for k,m in enumerate(ddp.problem.runningModels):
+        #   m.differential.
         # Reset x0 to measured state + warm-start solution
         ddp.problem.x0 = sim_data['X_mea_SIMU'][i, :]
         xs_init = list(ddp.xs[1:]) + [ddp.xs[-1]]

demos/iiwa_ocp_contact.py
@@ -61,19 +61,25 @@ import pinocchio as pin
 f_ext = []
 for i in range(nq+1):
     # CONTACT --> WORLD
-    W_X_ct = M_ct.action
+    W_M_ct = M_ct.copy()
+    f_WORLD = W_M_ct.actionInverse.T.dot(np.asarray(config['f_des']))
+    # W_X_ct = contact_placement.action
     # WORLD --> JOINT
-    j_X_W  = robot.data.oMi[i].actionInverse
+    j_M_W = robot.data.oMi[i].copy().inverse()
+    f_JOINT = j_M_W.actionInverse.T.dot(f_WORLD)
+    # j_X_W  = robot.data.oMi[i].actionInverse
     # CONTACT --> JOINT
-    j_X_ee = W_X_ct.dot(j_X_W)
-    # ADJOINT INVERSE (wrenches)
-    f_joint = j_X_ee.T.dot(np.asarray(config['f_des']))
-    f_ext.append(pin.Force(f_joint))
+    # j_X_ee = W_X_ct.dot(j_X_W)
+    # ADJOINT INVERSE (i.e. wrench JOINT --> CONTACT) 
+    # f_joint = j_X_ee.actionInverse.T.dot(np.asarray(config['f_des']))
+    f_ext.append(pin.Force(f_JOINT))
 # print(f_ext)
 u0 = pin_utils.get_tau(q0, v0, np.zeros((nq,1)), f_ext, robot.model)
 ug = pin_utils.get_u_grav(q0, robot)
 print("u0 = ", u0)
 print("ug = ", ug)
+
+
 
 # solver
 ddp = ocp_utils.init_DDP(robot, config, x0, callbacks=True,

demos/raisim_test_contact.py
@@ -1,6 +1,6 @@
 import numpy as np
 import time
-from utils import raisim_utils, path_utils
+from utils import raisim_utils, path_utils, pin_utils
 np.set_printoptions(precision=4, linewidth=180)
 
 # # # # # # # # # # # #
@@ -35,18 +35,43 @@ print("Initial placement : \n")
 print(M_ee)
 
 # Add a ball close to EE
-ball = env.world.addSphere(.01,100, 'default', 1)
-offset = np.array([0.,0.,0.08])
-p = robot.rai_robot.getFramePosition('EE')
-R = robot.rai_robot.getFrameOrientation('EE')
-pb = p + R.dot(offset)
-ball.setPosition(p)
+contact_placement = robot.data.oMf[id_endeff].copy()
+offset = 0.035
+contact_placement.translation = contact_placement.act(np.array([0., 0., offset])) 
+import pinocchio as pin
+ball = env.display_contact_surface(contact_placement, radius=0.1)
 
+# env.step()
+# Compute desired external force acting on each joint 
+f_ext = []
+for i in range(nq+1):
+    # CONTACT --> WORLD
+    W_X_ct = contact_placement.action
+    # WORLD --> JOINT
+    j_X_W  = robot.data.oMi[i].actionInverse
+    # CONTACT --> JOINT
+    j_X_ee = W_X_ct.dot(j_X_W)
+    # ADJOINT INVERSE (wrenches)
+    f_joint = j_X_ee.T.dot([0., 0., -1, 0., 0., 0.])
+    f_ext.append(pin.Force(f_joint))
+
+ug = pin_utils.get_u_grav(q0, robot)
+u0 = pin_utils.get_tau(q0, v0, np.zeros((nq,1)), f_ext, robot.model)
+print("ug = ", ug)
+print("u0 = ", u0)
 # step 
-env.step()
+for i in range(10000):
+  robot.send_joint_command(u0)
+  env.step()
+  time.sleep(0.01)
+  print(robot.get_contact_forces())
 # Check contacts
-for c in robot.get_contact_points():
-  print("local body idx : ", c.getlocalBodyIndex())
-  print("normal         : ", c.getNormal())
-  print("frame          : \n", c.getContactFrame())
-  print("Impulse        : ", c.getImpulse())
+# for c in robot.get_contact_points():
+#   print("local body idx : ", c.getlocalBodyIndex())
+#   print("normal         : ", c.getNormal())
+#   print("frame          : \n", c.getContactFrame())
+#   print("Impulse        : ", c.getImpulse())
+
+# print(robot.get_contact_forces())
+
+time.sleep(1000)

utils/raisim_utils.py
@@ -112,8 +112,8 @@ class PinRaiRobotWrapper:
         """    
         F = np.zeros(6)
         nb_contact_points = 0
-        # Get contact placement of contact point on the robot
-        M_ee = self.data.oMf[self.model.getFrameId('contact')].inverse() # W-->L
+        # Get contact frame placement of the robot's EE (tennis ball)
+        M_ee = self.data.oMf[self.model.getFrameId('contact')]
         for contact in self.get_contact_points():
             if(contact.getlocalBodyIndex() == 7):
                 nb_contact_points +=1
@@ -121,18 +121,13 @@ class PinRaiRobotWrapper:
                 R_contact = contact.getContactFrame().T   # !! careful need to transpose according to doc
                 p_contact = contact.get_position()
                 M_contact = pin.SE3(R_contact, p_contact) # L-->W
-                # print("Contact n°"+str(nb_contact_points))
-                # print("  ", contact.getImpulse())
-                # print("  ", contact.getDepth())
-                # print("Normal in local ", R.T.dot(contact.getNormal())) OK
-                # Contact frame 
-                f_local_raisim = contact.getImpulse() / self.world.getTimeStep() # Already in local frame of contact point in simulator
-                wrench_local_raisim = np.concatenate([f_local_raisim, np.zeros(3)])
-                # Torque induced at EE frame origin by force acting on contact frame
-                # Move to contact frame as defined in URDF : LOCAL(raisim) --> WORLD --> LOCAL(urdf)
-                wrench_world = M_contact.actionInverse.T.dot(wrench_local_raisim) # LOCAL --> WORLD Same as M_contact.act( pin.Force(w) )
-                wrench_ee = M_ee.action.T.dot(wrench_world)                       # WORLD --> LOCAL Same as M_contact.actInv( pin.Force(w) )
-                F += wrench_ee
+                # Wrench in local contact frame
+                f_local_raisim = contact.getImpulse() / self.world.getTimeStep() 
+                wrench_local_raisim = pin.Force(np.concatenate([f_local_raisim, np.zeros(3)]))
+                # Wrench in contact frame as defined in URDF (tennis ball): LOCAL(raisim) --> WORLD --> LOCAL(urdf)
+                wrench_world = M_contact.act(wrench_local_raisim) # LOCAL --> WORLD Same as M_contact.act( pin.Force(w) )
+                wrench_ee = M_ee.actInv(wrench_world)                       # WORLD --> LOCAL Same as M_contact.actInv( pin.Force(w) )
+                F += wrench_ee.vector
         return F
 
     def get_current_contacts(self):
@@ -362,7 +357,7 @@ class RaiEnv:
         # quat = list(pin.se3ToXYZQUAT(placement))
         # wall.setOrientation(quat[0], quat[1], quat[2], quat[3])
         # wall.setBodyType(raisim.BodyType.STATIC)
-
+        return wall
 
 
 # Load KUKA arm in Raisim environment