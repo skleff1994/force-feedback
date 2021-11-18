387628f8f085dca85624a839ad2c62be11f85fc5
Sebastien Kleff <skleff@akanagi.laas.fr>
2021-11-18 19:08:46 +0100

    visu lpf ocp circle ok

--- demos/iiwa_LPF_circle_OCP.py
+++ demos/iiwa_LPF_circle_OCP.py
@@ -82,8 +82,10 @@ if(WARM_START_IK):
     for k,m in enumerate(list(ddp.problem.runningModels) + [ddp.problem.terminalModel]):
         ref = m.differential.costs.costs['translation'].cost.residual.reference
         q_ws, v_ws, eps = pin_utils.IK_position(robot, q_ws, id_endeff, ref, DT=1e-2, IT_MAX=100)
-        xs_init.append(np.concatenate([q_ws, v_ws]))
-    us_init = [pin_utils.get_u_grav(xs_init[i][:nq], robot.model) for i in range(N_h)]
+        tau_ws = pin_utils.get_u_grav(q_ws, robot.model)
+        xs_init.append(np.concatenate([q_ws, v_ws, tau_ws]))
+        if(k<N_h):
+            us_init.append(tau_ws)
 
 # Classical warm start using initial config
 else:
@@ -101,41 +103,116 @@ if(PLOT):
     fig, ax = plot_utils.plot_ddp_results(ddp_data, which_plots=['p'], markers=['.'], colors=['b'], SHOW=True)
 
 
-# Visualize
+
 VISUALIZE = True
-pause = 0.05 # in s
+pause = 0.01 # in s
 if(VISUALIZE):
     import time
     import pinocchio as pin
-    robot.initDisplay(loadModel=True)
+    models = list(ddp.problem.runningModels) + [ddp.problem.terminalModel]
+    # Init viewer
+    robot.initViewer(loadModel=True)
     robot.display(q0)
-    viewer = robot.viz.viewer
-    log_rate = int(N_h/10)
-    draw_rate = int(N_h/100)
-    logger.info("Visualizing...")
-    time.sleep(1)
-    # Clean previous node if any
-    for i in range(N_h):
+    viewer = robot.viz.viewer; gui = viewer.gui
+
+    draw_rate = int(N_h/50)
+    log_rate  = int(N_h/10)
+    
+    ref_color  = [1., 0., 0., 1.]
+    real_color = [0., 0., 1., 0.3]
+    
+    ref_size    = 0.01
+    real_size   = 0.02
+    wrench_coef = 0.02
+
+    # Display reference trajectory as red spheres
+    if('translation' in config['WHICH_COSTS']):
+
+        # Remove circle ref traj and EE traj if already displayed
+        for i in range(N_h):
+            if(viewer.gui.nodeExists('world/EE_ref'+str(i))):
+                viewer.gui.deleteNode('world/EE_ref'+str(i), True)
+        
+        viewer.gui.refresh()
+
+    # Display EE trajectory as blue spheres
+    for i in range(N_h):      
         if(viewer.gui.nodeExists('world/EE_'+str(i))):
             viewer.gui.deleteNode('world/EE_'+str(i), True)
-    if(viewer.gui.nodeExists('world/ee')):
-        viewer.gui.deleteNode('world/ee', True)
-    viewer.gui.addSphere('world/ee', .03, [0. ,1. ,0, 1.])
-    viewer.gui.addLandmark('world/ee', .5)
-    viewer.gui.applyConfiguration('world/ee', list(pin.SE3ToXYZQUAT(M_ee.copy())))
+
     viewer.gui.refresh()
+    
+    logger.info("Visualizing...")
+
+    time.sleep(1.)
+
     for i in range(N_h):
-        viewer.gui.refresh()
-        robot.display(ddp.xs[i][:nq])
+        # Display robot in config q
+        q = ddp.xs[i][:nq]
+        robot.display(q)
+
+        # Display EE traj and ref circle traj
         if(i%draw_rate==0):
-            m_ee = M_ee.copy()
-            m_ee.translation = models[i].differential.costs.costs['translation'].cost.residual.reference
+            if('translation' in config['WHICH_COSTS']):
+                # EE ref circle trajectory
+                m_ee_ref = M_ee.copy()
+                m_ee_ref.translation = models[i].differential.costs.costs['translation'].cost.residual.reference
+                tf_ee_ref = list(pin.SE3ToXYZQUAT(m_ee_ref))
+                viewer.gui.addSphere('world/EE_ref'+str(i), ref_size, ref_color)
+                viewer.gui.applyConfiguration('world/EE_ref'+str(i), tf_ee_ref)
+            # EE trajectory
+            robot.framesForwardKinematics(q)
+            m_ee = robot.data.oMf[id_endeff].copy()
             tf_ee = list(pin.SE3ToXYZQUAT(m_ee))
-            viewer.gui.addSphere('world/EE_'+str(i), .01, [1. ,0 ,0, 1.])
+            viewer.gui.addSphere('world/EE_'+str(i), real_size, real_color)
             viewer.gui.applyConfiguration('world/EE_'+str(i), tf_ee)
+        
+
+        viewer.gui.refresh()
+
         if(i%log_rate==0):
             logger.info("Display config n°"+str(i))
+
         time.sleep(pause)
+
+
+
+# # Visualize
+# VISUALIZE = True
+# pause = 0.05 # in s
+# if(VISUALIZE):
+#     models = list(ddp.problem.runningModels) + [ddp.problem.terminalModel]
+#     import time
+#     import pinocchio as pin
+#     robot.initViewer(loadModel=True)
+#     robot.display(q0)
+#     viewer = robot.viz.viewer
+#     log_rate = int(N_h/10)
+#     draw_rate = int(N_h/100)
+#     logger.info("Visualizing...")
+#     time.sleep(1)
+#     # Clean previous node if any
+#     for i in range(N_h):
+#         if(viewer.gui.nodeExists('world/EE_'+str(i))):
+#             viewer.gui.deleteNode('world/EE_'+str(i), True)
+#     if(viewer.gui.nodeExists('world/ee')):
+#         viewer.gui.deleteNode('world/ee', True)
+#     viewer.gui.addSphere('world/ee', .03, [0. ,1. ,0, 1.])
+#     viewer.gui.addLandmark('world/ee', .5)
+#     viewer.gui.applyConfiguration('world/ee', list(pin.SE3ToXYZQUAT(M_ee.copy())))
+#     viewer.gui.refresh()
+#     for i in range(N_h):
+#         viewer.gui.refresh()
+#         robot.display(ddp.xs[i][:nq])
+#         if(i%draw_rate==0):
+#             m_ee = M_ee.copy()
+#             m_ee.translation = models[i].differential.costs.costs['translation'].cost.residual.reference
+#             tf_ee = list(pin.SE3ToXYZQUAT(m_ee))
+#             viewer.gui.addSphere('world/EE_'+str(i), .01, [1. ,0 ,0, 1.])
+#             viewer.gui.applyConfiguration('world/EE_'+str(i), tf_ee)
+#         if(i%log_rate==0):
+#             logger.info("Display config n°"+str(i))
+#         time.sleep(pause)
 
 # EE_ref_LOCAL = np.zeros(EE_ref.shape)
 # for i in range(EE_ref.shape[0]):
