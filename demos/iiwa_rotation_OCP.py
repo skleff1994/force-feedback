7c120aefc9174d370688845586b60f982686f3c9
Sebastien Kleff <skleff@akanagi.laas.fr>
2021-11-23 11:33:24 +0100

    fixed rpy, R in pin_utils + enables 1D array input for get_{p,v,R,rpy} functions

---
+++ demos/iiwa_rotation_OCP.py
@@ -0,0 +1,176 @@
+"""
+@package force_feedback
+@file iiwa_rotation_OCP.py
+@author Sebastien Kleff
+@license License BSD-3-Clause
+@copyright Copyright (c) 2020, New York University and Max Planck Gesellschaft.
+@date 2020-05-18
+@brief OCP for tracking EE rotation with the KUKA iiwa 
+"""
+
+'''
+The robot is tasked with rotation its EE about normal axis
+Trajectory optimization using Crocoddyl
+The goal of this script is to setup OCP (a.k.a. play with weights)
+'''
+
+import numpy as np  
+from utils import path_utils, ocp_utils, pin_utils, plot_utils, data_utils
+from robot_properties_kuka.config import IiwaConfig
+np.set_printoptions(precision=4, linewidth=180)
+import time
+
+import logging
+FORMAT_LONG   = '[%(levelname)s] %(name)s:%(lineno)s -> %(funcName)s() : %(message)s'
+FORMAT_SHORT  = '[%(levelname)s] %(name)s : %(message)s'
+logging.basicConfig(format=FORMAT_SHORT)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+
+# # # # # # # # # # # #
+### LOAD ROBOT MODEL ## 
+# # # # # # # # # # # # 
+# Read config file
+config = path_utils.load_config_file('iiwa_rotation_OCP')
+q0 = np.asarray(config['q0'])
+v0 = np.asarray(config['dq0'])
+x0 = np.concatenate([q0, v0])   
+# Get pin wrapper
+robot = IiwaConfig.buildRobotWrapper()
+# Get initial frame placement + dimensions of joint space
+id_endeff = robot.model.getFrameId('contact')
+nq, nv = robot.model.nq, robot.model.nv
+nx = nq+nv
+nu = nq
+# Update robot model with initial state
+robot.framesForwardKinematics(q0)
+robot.computeJointJacobians(q0)
+M_ee = robot.data.oMf[id_endeff]
+
+
+
+# # # # # # # # # 
+### OCP SETUP ###
+# # # # # # # # # 
+N_h = config['N_h']
+dt = config['dt']
+# Setup Croco OCP and create solver
+ddp = ocp_utils.init_DDP(robot, config, x0, callbacks=True, 
+                                            WHICH_COSTS=config['WHICH_COSTS']) 
+# Setup tracking problem with oritantation ref for EE trajectory
+models = list(ddp.problem.runningModels) + [ddp.problem.terminalModel]
+OMEGA  = config['frameCircleTrajectoryVelocity']
+for k,m in enumerate(models):
+    # Ref
+    t = min(k*config['dt'], 2*np.pi/OMEGA)
+    R_ee_ref = ocp_utils.rotation_matrix_WORLD(t, M_ee, 
+                                               omega=OMEGA)
+    # Cost translation
+    m.differential.costs.costs['translation'].cost.residual.reference = p_ee_ref
+    
+
+# Warm start state = IK of circle trajectory
+WARM_START_IK = True
+if(WARM_START_IK):
+    logger.info("Computing warm-start using Inverse Kinematics...")
+    xs_init = [] 
+    us_init = []
+    q_ws = q0
+    for k,m in enumerate(models):
+        ref = m.differential.costs.costs['translation'].cost.residual.reference
+        q_ws, v_ws, eps = pin_utils.IK_position(robot, q_ws, id_endeff, ref, DT=1e-2, IT_MAX=100)
+        xs_init.append(np.concatenate([q_ws, v_ws]))
+    us_init = [pin_utils.get_u_grav(xs_init[i][:nq], robot.model) for i in range(N_h)]
+
+# Classical warm start using initial config
+else:
+    ug  = pin_utils.get_u_grav(q0, robot.model)
+    xs_init = [x0 for i in range(config['N_h']+1)]
+    us_init = [ug for i in range(config['N_h'])]
+
+# Solve 
+ddp.solve(xs_init, us_init, maxiter=config['maxiter'], isFeasible=False)
+
+#  Plot
+PLOT = True
+if(PLOT):
+    ddp_data = data_utils.extract_ddp_data(ddp)
+    fig, ax = plot_utils.plot_ddp_results(ddp_data, which_plots=['p'], markers=['.'], colors=['b'], SHOW=True)
+
+
+VISUALIZE = True
+pause = 0.02 # in s
+if(VISUALIZE):
+    import time
+    import pinocchio as pin
+    models = list(ddp.problem.runningModels) + [ddp.problem.terminalModel]
+    # Init viewer
+    robot.initViewer(loadModel=True)
+    robot.display(q0)
+    viewer = robot.viz.viewer; gui = viewer.gui
+
+    draw_rate = int(N_h/50)
+    log_rate  = int(N_h/10)
+    
+    ref_color  = [1., 0., 0., 1.]
+    real_color = [0., 0., 1., 0.3]
+    
+    ref_size    = 0.01
+    real_size   = 0.02
+    wrench_coef = 0.02
+
+    # Display reference trajectory as red spheres
+    if('translation' or 'placement' in config['WHICH_COSTS']):
+
+        # Remove circle ref traj and EE traj if already displayed
+        for i in range(N_h):
+            if(viewer.gui.nodeExists('world/EE_ref'+str(i))):
+                viewer.gui.deleteNode('world/EE_ref'+str(i), True)
+        
+        viewer.gui.refresh()
+
+    # Display EE trajectory as blue spheres
+    for i in range(N_h):      
+        if(viewer.gui.nodeExists('world/EE_'+str(i))):
+            viewer.gui.deleteNode('world/EE_'+str(i), True)
+
+    viewer.gui.refresh()
+    
+    logger.info("Visualizing...")
+
+    time.sleep(1.)
+
+    for i in range(N_h):
+        # Display robot in config q
+        q = ddp.xs[i][:nq]
+        robot.display(q)
+
+        # Display EE traj and ref circle traj
+        if(i%draw_rate==0):
+            if('translation' or 'placement' in config['WHICH_COSTS']):
+                # EE ref circle trajectory
+                m_ee_ref = M_ee.copy()
+                if('translation' in config['WHICH_COSTS']):
+                    m_ee_ref.translation = models[i].differential.costs.costs['translation'].cost.residual.reference
+                elif('placement' in config['WHICH_COSTS']):
+                    m_ee_ref = models[i].differential.costs.costs['placement'].cost.residual.reference.copy()
+                tf_ee_ref = list(pin.SE3ToXYZQUAT(m_ee_ref))
+                viewer.gui.addSphere('world/EE_ref'+str(i), ref_size, ref_color)
+                viewer.gui.applyConfiguration('world/EE_ref'+str(i), tf_ee_ref)
+            # EE trajectory
+            robot.framesForwardKinematics(q)
+            m_ee = robot.data.oMf[id_endeff].copy()
+            tf_ee = list(pin.SE3ToXYZQUAT(m_ee))
+            viewer.gui.addSphere('world/EE_'+str(i), real_size, real_color)
+            viewer.gui.applyConfiguration('world/EE_'+str(i), tf_ee)
+        
+
+        viewer.gui.refresh()
+
+        if(i%log_rate==0):
+            logger.info("Display config n°"+str(i))
+
+        time.sleep(pause)
--- utils/pin_utils.py
+++ utils/pin_utils.py
@@ -6,6 +6,8 @@ from numpy.linalg import pinv
 import time
 
 import logging
+
+from pinocchio.deprecated import se3ToXYZQUAT
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -27,13 +29,19 @@ def get_p_(q, model, id_endeff):
         model     : pinocchio model
         id_endeff : id of EE frame
     '''
-    N = np.shape(q)[0]
-    p = np.empty((N,3))
+    
     data = model.createData()
-    for i in range(N):
-        pin.forwardKinematics(model, data, q[i])
+    if(type(q)==np.ndarray and len(q.shape)==1):
+        pin.forwardKinematics(model, data, q)
         pin.updateFramePlacements(model, data)
-        p[i,:] = data.oMf[id_endeff].translation.T
+        p = data.oMf[id_endeff].translation.T
+    else:
+        N = np.shape(q)[0]
+        p = np.empty((N,3))
+        for i in range(N):
+            pin.forwardKinematics(model, data, q[i])
+            pin.updateFramePlacements(model, data)
+            p[i,:] = data.oMf[id_endeff].translation.T
     return p
 
 
@@ -55,43 +63,78 @@ def get_v_(q, dq, model, id_endeff):
         model     : pinocchio model
         id_endeff : id of EE frame
     '''
-    N = np.shape(q)[0]
-    v = np.empty((N,3))
     data = model.createData()
-    for i in range(N):
-        # Get jacobian + compute vel
-        J = pin.computeFrameJacobian(model, data, q[i,:], id_endeff)
-        v[i,:] = J.dot(dq[i])[:3] # expressed in LOCAL frame 
+    if(len(q) != len(v)):
+        logger.error("q and v must have the same size !")
+    if(type(q)==np.ndarray and len(q.shape)==1):
+        J = pin.computeFrameJacobian(model, data, q, id_endeff)
+        v = J.dot(dq)[:3] 
+    else:
+        N = np.shape(q)[0]
+        v = np.empty((N,3))
+        for i in range(N):
+            J = pin.computeFrameJacobian(model, data, q[i,:], id_endeff)
+            v[i,:] = J.dot(dq[i])[:3]  
     return v
 
 
-def get_R(q, dq, pin_robot, id_endeff):
+def get_R(q, pin_robot, id_endeff):
     '''
-    Returns end-effector velocities given q,dq trajectory 
+    Returns end-effector rotation matrices given q trajectory 
         q         : joint positions
-        dq        : joint velocities
         pin_robot : pinocchio wrapper
         id_endeff : id of EE frame
     '''
-    return get_R_(q, dq, pin_robot.model, id_endeff)
-
+    return get_R_(q, pin_robot.model, id_endeff)
 
 def get_R_(q, model, id_endeff):
     '''
-    Returns end-effector rotations given q
+    Returns end-effector rotation matrices given q trajectory
         q         : joint positions
-        dq        : joint velocities
+        model     : pinocchio model
+        id_endeff : id of EE frame
+    Output : single 3x3 array (or list of 3x3 arrays)
+    '''
+    data = model.createData()
+    if(type(q)==np.ndarray and len(q.shape)==1):
+        pin.framesForwardKinematics(model, data, q)
+        R = data.oMf[id_endeff].rotation.copy()
+    else:
+        N = np.shape(q)[0]
+        R = []    
+        for i in range(N):    
+            pin.framesForwardKinematics(model, data, q[i])
+            R.append(data.oMf[id_endeff].rotation.copy())
+    return R
+
+
+
+def get_rpy(q, pin_robot, id_endeff):
+    '''
+    Returns RPY angles of end-effector frame given q trajectory
+        q         : joint positions
+        model     : pinocchio wrapper
+        id_endeff : id of EE frame
+    '''
+    return get_rpy_(q, pin_robot.model, id_endeff)
+
+
+def get_rpy_(q, model, id_endeff):
+    '''
+    Returns RPY angles of end-effector frame given q trajectory
+        q         : joint positions
         model     : pinocchio model
         id_endeff : id of EE frame
     '''
-    N = np.shape(q)[0]
-    R = []
-    data = model.createData()
-    for i in range(N):
-        # Get jacobian + compute vel
-        pin.framesForwardKinematics(q)
-        R.append(data.oMf[id_endeff].rotation.copy())
-    return R
+    R = get_R_(q, model, id_endeff)
+    if(type(R)==list):
+        N = np.shape(q)[0]
+        rpy = np.empty((N,3))
+        for i in range(N):
+            rpy[i,:] = pin.utils.matrixToRpy(R[i])
+    else:
+        rpy = pin.utils.matrixToRpy(R)
+    return rpy
 
 
 def get_Rdot(q, dq, pin_robot, id_endeff):
@@ -104,23 +147,6 @@ def get_Rdot(q, dq, pin_robot, id_endeff):
     '''
     return get_R_(q, dq, pin_robot.model, id_endeff)
 
-
-def get_R_(q, model, id_endeff):
-    '''
-    Returns end-effector rotations given q
-        q         : joint positions
-        dq        : joint velocities
-        model     : pinocchio model
-        id_endeff : id of EE frame
-    '''
-    N = np.shape(q)[0]
-    R = []
-    data = model.createData()
-    for i in range(N):
-        # Get jacobian + compute vel
-        pin.framesForwardKinematics(q)
-        R.append(data.oMf[id_endeff].rotation.copy())
-    return R
 
 
 
