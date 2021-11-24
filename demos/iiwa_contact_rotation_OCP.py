fixed issue with force ref contact 3D and OCP rotation contact ok

demos/iiwa_contact_rotation_OCP.py
@@ -49,7 +49,7 @@ nu = nq
 # Update robot model with initial state
 robot.framesForwardKinematics(q0)
 robot.computeJointJacobians(q0)
-M_ee = robot.data.oMf[id_endeff]
+M_ee = robot.data.oMf[id_endeff].copy()
 
 
 # # # # # # # # # 
@@ -63,20 +63,22 @@ ddp = ocp_utils.init_DDP(robot, config, x0, callbacks=True,
 # Setup tracking problem with oritantation ref for EE trajectory
 models = list(ddp.problem.runningModels) + [ddp.problem.terminalModel]
 OMEGA  = config['frameRotationTrajectoryVelocity']
-# for k,m in enumerate(models):
-#     # Ref
-#     t = min(k*config['dt'], 2*np.pi/OMEGA)
-#     # Desired RPY in WORLD frame
-#     R_ee_ref_WORLD = M_ee.rotation.copy().dot(pin.utils.rpyToMatrix(np.array([0., 0., np.sin(OMEGA*t)])))
-#     # Cost translation
-#     m.differential.costs.costs['rotation'].cost.residual.reference = R_ee_ref_WORLD
-#     # Contact model
-#     m.differential.contacts.contacts["contact"].contact.reference = M_ee.translation.copy() 
+for k,m in enumerate(models):
+    # Ref
+    t = min(k*config['dt'], 2*np.pi/OMEGA)
+    # Desired RPY in WORLD frame
+    rpy_LOCAL = np.array([0., 0., np.sin(OMEGA*t)])
+    # rpy_LOCAL = np.array([0., 0., 1.])
+    R_ee_ref_WORLD = M_ee.rotation.copy().dot(pin.utils.rpyToMatrix(rpy_LOCAL))
+    # Cost rotation EE
+    m.differential.costs.costs['rotation'].cost.residual.reference = R_ee_ref_WORLD
+    # Contact model
+    m.differential.contacts.contacts["contact"].contact.reference = M_ee.translation.copy()
 
 
 
 # Warm start state = IK of circle trajectory
-WARM_START_IK = False
+WARM_START_IK = True
 if(WARM_START_IK):
     logger.info("Computing warm-start using Inverse Kinematics...")
     xs_init = [] 
@@ -88,7 +90,7 @@ if(WARM_START_IK):
         Mref.translation = M_ee.translation.copy() 
         # Get corresponding forces at each joint
         f_ext = pin_utils.get_external_joint_torques(Mref, config['frameForceRef'], robot)
-        q_ws, v_ws, eps = pin_utils.IK_placement(robot, q_ws, id_endeff, Mref, DT=1e-2, IT_MAX=10)
+        q_ws, v_ws, eps = pin_utils.IK_placement(robot, q_ws, id_endeff, Mref, DT=1e-2, IT_MAX=10, LOGS=False)
         xs_init.append(np.concatenate([q_ws, v_ws]))
         if(k<N_h):
             us_init.append(pin_utils.get_tau(q_ws, v_ws, np.zeros((nq,1)), f_ext, robot.model))

utils/ocp_utils.py
@@ -307,13 +307,11 @@ def init_DDP(robot, config, x0, callbacks=False,
     actuation = crocoddyl.ActuationModelFull(state)
   
   # Contact or not ?
-    # CONTACT6D = False
-    # CONTACT3D = False
     CONTACT      = False
     CONTACT_TYPE = 'None'
-    # 6D ?
     if('contactModelFrameName' in config.keys()):
       CONTACT = True
+      # 6D or 3D ?
       if('contactModelRotationRef' in config.keys()):
         CONTACT_TYPE = '6D'
       else:
@@ -521,37 +519,37 @@ def init_DDP(robot, config, x0, callbacks=False,
           if(not CONTACT):
             logger.error("Force cost but no contact model is defined ! ")
           # 6D contact case
-          # if(CONTACT_TYPE=='6D'):
-          # Default force reference = zero force
-          if(config['frameForceRef']=='DEFAULT'):
-            frameForceRef = pin.Force( np.zeros(6) )
-          else:
-            frameForceRef = pin.Force( np.asarray(config['frameForceRef']) )
-          frameForceWeights = np.asarray(config['frameForceWeights'])
-          frameForceFrameId = robot.model.getFrameId(config['frameForceFrameName'])
-          frameForceCost = crocoddyl.CostModelResidual(state, 
-                                                      crocoddyl.ActivationModelWeightedQuad(frameForceWeights**2), 
-                                                      crocoddyl.ResidualModelContactForce(state, 
-                                                                                          frameForceFrameId, 
-                                                                                          frameForceRef, 
-                                                                                          6, 
-                                                                                          actuation.nu))
-          # # 3D contact case
-          # elif(CONTACT_TYPE=='3D'):
-          #   # Default force reference = zero force
-          #   if(config['frameForceRef']=='DEFAULT'):
-          #     frameForceRef = np.zeros(3)
-          #   else:
-          #     frameForceRef = np.asarray(config['frameForceRef'])[:3]
-          #   frameForceWeights = np.asarray(config['frameForceWeights'])
-          #   frameForceFrameId = robot.model.getFrameId(config['frameForceFrameName'])
-          #   frameForceCost = crocoddyl.CostModelResidual(state, 
-          #                                               crocoddyl.ActivationModelWeightedQuad(frameForceWeights**2), 
-          #                                               crocoddyl.ResidualModelContactForce(state, 
-          #                                                                                   frameForceFrameId, 
-          #                                                                                   frameForceRef, 
-          #                                                                                   3, 
-          #                                                                                   actuation.nu))
+          if(CONTACT_TYPE=='6D'):
+            # Default force reference = zero force
+            if(config['frameForceRef']=='DEFAULT'):
+              frameForceRef = pin.Force( np.zeros(6) )
+            else:
+              frameForceRef = pin.Force( np.asarray(config['frameForceRef']) )
+            frameForceWeights = np.asarray(config['frameForceWeights'])
+            frameForceFrameId = robot.model.getFrameId(config['frameForceFrameName'])
+            frameForceCost = crocoddyl.CostModelResidual(state, 
+                                                        crocoddyl.ActivationModelWeightedQuad(frameForceWeights**2), 
+                                                        crocoddyl.ResidualModelContactForce(state, 
+                                                                                            frameForceFrameId, 
+                                                                                            frameForceRef, 
+                                                                                            6, 
+                                                                                            actuation.nu))
+          # 3D contact case
+          elif(CONTACT_TYPE=='3D'):
+            # Default force reference = zero force
+            if(config['frameForceRef']=='DEFAULT'):
+              frameForceRef = pin.Force( np.zeros(6) )
+            else:
+              frameForceRef = pin.Force( np.asarray(config['frameForceRef']) )
+            frameForceWeights = np.asarray(config['frameForceWeights'])[:3]
+            frameForceFrameId = robot.model.getFrameId(config['frameForceFrameName'])
+            frameForceCost = crocoddyl.CostModelResidual(state, 
+                                                        crocoddyl.ActivationModelWeightedQuad(frameForceWeights**2), 
+                                                        crocoddyl.ResidualModelContactForce(state, 
+                                                                                            frameForceFrameId, 
+                                                                                            frameForceRef, 
+                                                                                            3, 
+                                                                                            actuation.nu))
           # Add cost term to IAM
           runningModels[i].differential.costs.addCost("force", frameForceCost, config['frameForceWeight'])
         # Friction cone 

utils/pin_utils.py
@@ -344,7 +344,7 @@ def IK_position(robot, q, frame_id, p_des, LOGS=False, DISPLAY=False, DT=1e-2, I
             break    
     return q, vq, errs
 
-def IK_placement(robot, q0, frame_id, oMf_des, DT=1e-2, IT_MAX=1000, EPS=1e-6, DAMP=1e-6):
+def IK_placement(robot, q0, frame_id, oMf_des, LOGS=False, DT=1e-2, IT_MAX=1000, EPS=1e-6, DAMP=1e-6):
     '''
     Inverse kinematics: returns q, v to reach desired placement M 
     '''
@@ -357,21 +357,16 @@ def IK_placement(robot, q0, frame_id, oMf_des, DT=1e-2, IT_MAX=1000, EPS=1e-6, D
     errs = []
     # Loop on an inverse kinematics for 200 iterations.
     for i in range(IT_MAX): 
+        if(i%10 == 0 and LOGS==True):
+            print("Step "+str(i)+"/"+str(IT_MAX))
         pin.framesForwardKinematics(model, data, q)  
         dMi = oMf_des.actInv(oMf)
         err = pin.log(dMi).vector
         errs.append(err)
-        if np.linalg.norm(err) < EPS:
-            success = True
-            break       
-        if i >= IT_MAX:
-            success = False
-            break
+        if(i%10 == 0 and LOGS==True):
+            print(np.linalg.norm(err))
         J = pin.computeFrameJacobian(model, data, q, frame_id)    
         vq = - J.T @ pinv(J.dot(J.T) + DAMP * np.eye(6)) @ err    
         # vq = - J.T.dot(np.linalg.solve(J.dot(J.T) + DAMP * np.eye(6), err))
         q = pin.integrate(model, q, vq * DT)
-        # robot.display(q)                                   
-        time.sleep(0.1)
-        i += 1
     return q, vq, errs