a375262aef89016b9b8e922e152bb4eae2be7fbc
Sébastien Kleff <sk8001@nyu.edu>
2022-04-29 12:12:31 +0200

    added a_partial_dx unittst

--- demos/contact/debug_DAMcontact_calcDiff_method2.py
+++ demos/contact/debug_DAMcontact_calcDiff_method2.py
@@ -31,7 +31,7 @@ RANDOM_SEED     = 1
 np.random.seed(RANDOM_SEED)
 
 # Test parameters 
-PIN_REFERENCE_FRAME         = pin.WORLD     
+PIN_REFERENCE_FRAME         = pin.LOCAL     
 ALIGN_LOCAL_WITH_WORLD      = False
 TORQUE_SUCH_THAT_ZERO_FORCE = False
 ZERO_JOINT_VELOCITY         = False
@@ -146,10 +146,11 @@ da0_dx_ND_1 = numdiff(lambda x_:contactCalc1(model, data, contactFrameId, x_, np
 da0_dx_1 = contactCalcDiff1(model, data, contactFrameId, x0)
 test_da0_dx_1 = np.allclose(da0_dx_1, da0_dx_ND_1, RTOL, ATOL)
 print(testcolormap[test_da0_dx_1] + "   -- Test da0_dx numdiff (1) drift : " + str(test_da0_dx_1) + bcolors.ENDC)
-if(not test_da0_dx_1):
-    print("analytic = \n", da0_dx_1)
-    print("numdiff \n", da0_dx_ND_1)
+# if(not test_da0_dx_1):
+#     print("analytic = \n", da0_dx_1)
+#     print("numdiff \n", da0_dx_ND_1)
 
+print("\n")
 
 # If we want to get the same results as in dam2.py, need to compute joint acc and force with forwardDynamics(Jc,a0)
 # and then update RNEA derivatives using joint acc, f_ext . It should be ckecked that in this case the classical acc is 0 
@@ -175,9 +176,9 @@ da0_dx_ND_1 = numdiff(lambda x_:contactCalc1(model, data, contactFrameId, x_, da
 da0_dx_1 = contactCalcDiff1(model, data, contactFrameId, x0)
 test_da0_dx_1 = np.allclose(da0_dx_1, da0_dx_ND_1, RTOL, ATOL)
 print(testcolormap[test_da0_dx_1] + "   -- Test da0_dx numdiff (1) with constraint: " + str(test_da0_dx_1) + bcolors.ENDC)
-if(not test_da0_dx_1):
-    print("analytic = \n", da0_dx_1)
-    print("numdiff \n", da0_dx_ND_1)
+# if(not test_da0_dx_1):
+#     print("analytic = \n", da0_dx_1)
+#     print("numdiff \n", da0_dx_ND_1)
 
 
 
@@ -204,10 +205,12 @@ def contactCalc2(model, data, frameId, x, a, ref):
 def contactCalcDiff2(model, data, frameId, x, ref):
     if(ref == pin.LOCAL):
         fJf = pin.getFrameJacobian(model, data, frameId, pin.LOCAL)
+        a = pin.getFrameAcceleration(model, data, frameId, pin.LOCAL)
         v_partial_dq, a_partial_dq, a_partial_dv, a_partial_da = pin.getFrameAccelerationDerivatives(model, data, frameId, pin.LOCAL) 
         v = pin.getFrameVelocity(model, data, frameId, pin.LOCAL)
     if(ref == pin.WORLD or ref == pin.LOCAL_WORLD_ALIGNED):
         fJf = pin.getFrameJacobian(model, data, frameId, pin.LOCAL_WORLD_ALIGNED)
+        a = pin.getFrameAcceleration(model, data, frameId, pin.LOCAL_WORLD_ALIGNED)
         v_partial_dq, a_partial_dq, a_partial_dv, a_partial_da = pin.getFrameAccelerationDerivatives(model, data, frameId, pin.LOCAL_WORLD_ALIGNED) 
         v = pin.getFrameVelocity(model, data, frameId, pin.LOCAL_WORLD_ALIGNED)
     vv = v.linear ; vw = v.angular
@@ -220,8 +223,8 @@ def contactCalcDiff2(model, data, frameId, x, ref):
     da0_dx[:,nv:] = a_partial_dv[:3,:]
     da0_dx[:,nv:] += pin.skew(vw) @ fJf[:3,:] 
     da0_dx[:,nv:] -= pin.skew(vv) @ fJf[3:,:]
+    # assert
     return da0_dx
-
 
 # Contact calcDiff : acceleration derivatives
 def contactCalcDiff2Bis(model, data, frameId, x, ref):
@@ -242,9 +245,14 @@ def contactCalcDiff2Bis(model, data, frameId, x, ref):
         da0_dx_temp = da0_dx.copy()
         R = data.oMf[frameId].rotation
         da0_dx = R @ da0_dx_temp
+        a0 = pin.getFrameClassicalAcceleration(model, data, frameId, pin.LOCAL_WORLD_ALIGNED).linear
+        Jw = pin.getFrameJacobian(model, data, frameId, pin.LOCAL_WORLD_ALIGNED)[3:,:]
+        # print("skew term = ", pin.skew(a0)@Jw)  0 if constraint is satisfied since a=0
+        da0_dx[:,:nq] -= pin.skew(a0)@Jw
     return da0_dx
 
 
+print("\n")
 
 # Checking calc and calcDiff with zero acceleration (no forces)
 frameId = contactFrameId
@@ -262,36 +270,101 @@ da0_dx_2bis = contactCalcDiff2Bis(model, data, contactFrameId, x0, PIN_REFERENCE
 test_da0_dx_2    = np.allclose(da0_dx_2, da0_dx_ND_2, RTOL, ATOL)
 test_da0_dx_2bis = np.allclose(da0_dx_2bis, da0_dx_ND_2, RTOL, ATOL)
 test_da0_dx_2crossed = np.allclose(da0_dx_2, da0_dx_2bis, RTOL, ATOL)
-print(testcolormap[test_da0_dx_2] + "   -- Test da0_dx numdiff (2): " + str(test_da0_dx_2) + bcolors.ENDC)
-if(not test_da0_dx_2):
-    print("analytic 2 = \n", da0_dx_2)
-    print("numdiff \n", da0_dx_ND_2)
-print(testcolormap[test_da0_dx_2bis] + "   -- Test da0_dx numdiff (2bis): " + str(test_da0_dx_2bis) + bcolors.ENDC)
-if(not test_da0_dx_2bis):
-    print("analytic 2bis = \n", da0_dx_2bis)
-    print("numdiff \n", da0_dx_ND_2)
-print(testcolormap[test_da0_dx_2crossed] + "   -- Test da0_dx crossed (2 and 2bis): " + str(test_da0_dx_2crossed) + bcolors.ENDC)
-if(not test_da0_dx_2crossed):
-    print("analytic 2 = \n", da0_dx_2)
-    print("analytic 2bis = \n", da0_dx_2bis)
+print(testcolormap[test_da0_dx_2] + "   -- Test da0_dx numdiff (2) drift : " + str(test_da0_dx_2) + bcolors.ENDC)
+# if(not test_da0_dx_2):
+#     print("analytic 2 = \n", da0_dx_2)
+#     print("numdiff \n", da0_dx_ND_2)
+print(testcolormap[test_da0_dx_2bis] + "   -- Test da0_dx numdiff (2bis) drift: " + str(test_da0_dx_2bis) + bcolors.ENDC)
+# if(not test_da0_dx_2bis):
+#     print("analytic 2bis = \n", da0_dx_2bis)
+#     print("numdiff \n", da0_dx_ND_2)
+print(testcolormap[test_da0_dx_2crossed] + "   -- Test da0_dx crossed (2 and 2bis) drift: " + str(test_da0_dx_2crossed) + bcolors.ENDC)
+# if(not test_da0_dx_2crossed):
+#     print("analytic 2 = \n", da0_dx_2)
+#     print("analytic 2bis = \n", da0_dx_2bis)
 
-# Checking calc and calcDiff with zero acceleration (no forces)
+
+print("\n")
+
+# Contact calcDiff : acceleration derivatives
+def a_partial_dx(model, data, frameId, x, ref):
+    if(ref == pin.LOCAL):
+        _, a_partial_dq, a_partial_dv, _ = pin.getFrameAccelerationDerivatives(model, data, frameId, pin.LOCAL)
+    if(ref == pin.WORLD or ref == pin.LOCAL_WORLD_ALIGNED):
+        _, a_partial_dq, a_partial_dv, _ = pin.getFrameAccelerationDerivatives(model, data, frameId, pin.LOCAL_WORLD_ALIGNED) 
+    a_partial_dx = np.hstack([ a_partial_dq, a_partial_dv ])
+    return a_partial_dx
+
+def linear_acc(model, data, frameId, x, a, ref):
+    pin.forwardKinematics(model,data,x[:nq],x[nq:nq+nv],a)
+    pin.updateFramePlacements(model,data)
+    if(ref == pin.WORLD or ref == pin.LOCAL_WORLD_ALIGNED):
+        a = pin.getFrameAcceleration(model,data,frameId,pin.LOCAL_WORLD_ALIGNED).linear
+    else:
+        a = pin.getFrameAcceleration(model,data,frameId,pin.LOCAL).linear
+    return a
+
+def classical_acc(model, data, frameId, x, a, ref):
+    pin.forwardKinematics(model,data,x[:nq],x[nq:nq+nv],a)
+    pin.updateFramePlacements(model,data)
+    if(ref == pin.WORLD or ref == pin.LOCAL_WORLD_ALIGNED):
+        a = pin.getFrameClassicalAcceleration(model,data,frameId,pin.LOCAL_WORLD_ALIGNED).linear
+    else:
+        a = pin.getFrameClassicalAcceleration(model,data,frameId,pin.LOCAL).linear
+    return a
+
+# Check that a_partial_dx matches the derivative of the acceleration
+linear_acc_ND = numdiff(lambda x_:linear_acc(model, data, frameId, x_, data.ddq, PIN_REFERENCE_FRAME), x0)
+a_partial_dx  = a_partial_dx(model, data, frameId, x0, PIN_REFERENCE_FRAME)[:3,:]
+test_partial_dx    = np.allclose(linear_acc_ND, a_partial_dx, RTOL, ATOL)
+print(testcolormap[test_partial_dx] + "   -- Test partial dx " + str(test_partial_dx) + bcolors.ENDC)
+# print("analytic : \n", a_partial_dx)
+# print("numdiff = \n", linear_acc_ND)
+# print("\n")
+
+print("\n")
+
+# If we want to get the same results as in dam2.py, need to compute joint acc and force with forwardDynamics(Jc,a0)
+# and then update RNEA derivatives using joint acc, f_ext . It should be ckecked that in this case the classical acc is 0 
+# since the constraint is resolved 
 frameId = contactFrameId
 model = robot.model
 data = robot.model.createData()
 pin.computeAllTerms(model, data, q0, v0)
-pin.forwardKinematics(model, data, q0, v0, np.zeros(nq))
-pin.computeForwardKinematicsDerivatives(model, data, q0, v0, np.zeros(nq))
-pin.computeJointJacobians(model, data, q0)
-pin.updateFramePlacements(model, data)
-pin.computeRNEADerivatives(model, data, q0, v0, np.zeros(nq)) 
-da0_dx_ND_1 = numdiff(lambda x_:contactCalc2(model, data, contactFrameId, x_, np.zeros(nq), PIN_REFERENCE_FRAME), x0)
-da0_dx_1 = contactCalcDiff2Bis(model, data, contactFrameId, x0, PIN_REFERENCE_FRAME)
-test_da0_dx_1 = np.allclose(da0_dx_1, da0_dx_ND_1, RTOL, ATOL)
-print(testcolormap[test_da0_dx_1] + "   -- Test da0_dx numdiff (2bis): " + str(test_da0_dx_1) + bcolors.ENDC)
-if(not test_da0_dx_1):
-    print("analytic = \n", da0_dx_1)
-    print("numdiff \n", da0_dx_ND_1)
+# Compute forward dynamics with drift obtained from contact model 
+    # Drift 
+a0 = contactCalc1(model, data, frameId, x0, np.zeros(nq))
+fJf = pin.getFrameJacobian(model, data, frameId, pin.LOCAL)
+pin.forwardDynamics(model, data, tau, fJf[:3,:], a0)
+    # get force at joint level
+fext = [pin.Force.Zero() for _ in range(model.njoints)]
+fext[model.frames[frameId].parent] = model.frames[frameId].placement.act(pin.Force(data.lambda_c, np.zeros(3)))
+    # Get derivatives
+pin.computeRNEADerivatives(model,data, q0, v0, data.ddq, fext)
+    # Check constraint acc = 0
+assert(np.linalg.norm(pin.getFrameClassicalAcceleration(model, data, frameId, pin.LOCAL).linear) <= 1e-6 )
+da0_dx_ND_2 = numdiff(lambda x_:contactCalc2(model, data, contactFrameId, x_, data.ddq, PIN_REFERENCE_FRAME), x0)
+da0_dx_2    = contactCalcDiff2(model, data, contactFrameId, x0, PIN_REFERENCE_FRAME)
+da0_dx_2bis = contactCalcDiff2Bis(model, data, contactFrameId, x0, PIN_REFERENCE_FRAME)
+test_da0_dx_2    = np.allclose(da0_dx_2, da0_dx_ND_2, RTOL, ATOL)
+test_da0_dx_2bis = np.allclose(da0_dx_2bis, da0_dx_ND_2, RTOL, ATOL)
+test_da0_dx_2crossed = np.allclose(da0_dx_2, da0_dx_2bis, RTOL, ATOL)
+print(testcolormap[test_da0_dx_2] + "   -- Test da0_dx numdiff (2) with constraint: " + str(test_da0_dx_2) + bcolors.ENDC)
+# if(not test_da0_dx_2):
+#     print("analytic 2 = \n", da0_dx_2)
+#     print("numdiff \n", da0_dx_ND_2)
+print(testcolormap[test_da0_dx_2bis] + "   -- Test da0_dx numdiff (2bis) constraint : " + str(test_da0_dx_2bis) + bcolors.ENDC)
+# if(not test_da0_dx_2bis):
+#     print("analytic 2bis = \n", da0_dx_2bis)
+#     print("numdiff \n", da0_dx_ND_2)
+print(testcolormap[test_da0_dx_2crossed] + "   -- Test da0_dx crossed (2 and 2bis) constraint : " + str(test_da0_dx_2crossed) + bcolors.ENDC)
+# if(not test_da0_dx_2crossed):
+#     print("analytic 2 = \n", da0_dx_2)
+#     print("analytic 2bis = \n", da0_dx_2bis)
+
+
+
+
 
 # # Forward dynamics rewritten with forces in world coordinates.
 # def fdyn(model, data, id_frame, x, tau, ref):
