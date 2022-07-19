b5b2ebefe575f3067c5b837ca316889b6a618f88
Sébastien Kleff <sk8001@nyu.edu>
2022-07-19 19:07:51 +0200

    need to debug mpc soft contact: wht's the feedback scheme

--- core_mpc/data.py
+++ core_mpc/data.py
@@ -700,7 +700,7 @@ class MPCDataHandlerAbstract:
     if('ctrlRegGrav' in self.WHICH_COSTS):
       q = self.state_pred[nb_plan, 0, :self.nq]
       self.ctrl_ref[nb_plan, :] = pin_utils.get_u_grav(q, m.differential.pinocchio, self.armature)
-    if('force' in self.WHICH_COSTS):
+    if('force' in self.WHICH_COSTS and hasattr(m.differential.costs.costs, 'force')):
       self.f_ee_ref[nb_plan, :] = m.differential.costs.costs['force'].cost.residual.reference.vector
     if('stateReg' in self.WHICH_COSTS):
       self.state_ref[nb_plan, :] = m.differential.costs.costs['stateReg'].cost.residual.reference
---
+++ demos/contact/soft_contact_MPC.py
@@ -0,0 +1,229 @@
+"""
+@package force_feedback
+@file contact_MPC.py
+@author Sebastien Kleff
+@license License BSD-3-Clause
+@copyright Copyright (c) 2021, New York University & LAAS-CNRS
+@date 2021-10-28
+@brief Closed-loop MPC for force task 
+"""
+
+'''
+The robot is tasked with exerting a constant normal force  
+Trajectory optimization using Crocoddyl in closed-loop MPC 
+(feedback from state x=(q,v), control u = tau) 
+Using PyBullet simulator & GUI for rigid-body dynamics + visualization
+
+The goal of this script is to simulate MPC with state feedback, optionally
+imperfect actuation (bias, noise, delays) at higher frequency
+'''
+
+
+import sys
+sys.path.append('.')
+
+from core_mpc.misc_utils import CustomLogger, GLOBAL_LOG_LEVEL, GLOBAL_LOG_FORMAT
+logger = CustomLogger(__name__, GLOBAL_LOG_LEVEL, GLOBAL_LOG_FORMAT).logger
+
+
+import numpy as np  
+np.set_printoptions(precision=4, linewidth=180)
+
+from core_mpc import path_utils, pin_utils, mpc_utils, misc_utils
+
+from soft_mpc.data import MPCDataHandlerSoftContact, DDPDataHanlderSoftContact
+from soft_mpc.ocp import OptimalControlProblemSoftContact
+
+
+
+def main(robot_name='iiwa', simulator='bullet', PLOT_INIT=False):
+
+
+  # # # # # # # # # # # # # # # # # # #
+  ### LOAD ROBOT MODEL and SIMU ENV ### 
+  # # # # # # # # # # # # # # # # # # # 
+  # Read config file
+  config, config_name = path_utils.load_config_file(__file__, robot_name)
+  # Create a simulation environment & simu-pin wrapper 
+  dt_simu = 1./float(config['simu_freq'])  
+  q0 = np.asarray(config['q0'])
+  v0 = np.asarray(config['dq0'])
+  x0 = np.concatenate([q0, v0])   
+  if(simulator == 'bullet'):
+    from core_mpc import sim_utils as simulator_utils
+    env, robot_simulator, base_placement = simulator_utils.init_bullet_simulation(robot_name, dt=dt_simu, x0=x0)
+    print(base_placement)
+    robot = robot_simulator.pin_robot
+  elif(simulator == 'raisim'):
+    from core_mpc import raisim_utils as simulator_utils
+    env, robot_simulator = simulator_utils.init_raisim_simulation(robot_name, dt=dt_simu, x0=x0)  
+    robot = robot_simulator
+  else:
+    logger.error('Please choose a simulator from ["bullet", "raisim"] !')
+  # Get dimensions 
+  nq, nv = robot.model.nq, robot.model.nv; nu = nq
+  # Placement of LOCAL end-effector frame w.r.t. WORLD frame
+  id_endeff = robot.model.getFrameId(config['frame_of_interest'])
+  ee_frame_placement = robot.data.oMf[robot.model.getFrameId(config['frame_of_interest'])]
+  oMf = robot.data.oMf[id_endeff]
+  # Contact model
+  # TODO: put in config file
+  oPc = oMf.translation + np.array([0.,0.,0.05    ])
+  Kp = 100
+  Kv = 2*np.sqrt(Kp)
+  # Placement of contact frame w.r.t. LOCAL frame
+  contact_placement = ee_frame_placement.copy()
+  # contact_placement.rotation 
+  M_ct = robot.data.oMf[id_endeff].copy()
+  contact_placement.translation =  contact_placement.act( np.asarray(config['contact_plane_offset']) ) 
+  simulator_utils.display_contact_surface(contact_placement, bullet_endeff_ids=robot_simulator.bullet_endeff_ids)
+  
+
+  import time
+  time.sleep(1)
+
+  # # # # # # # # # 
+  ### OCP SETUP ###
+  # # # # # # # # #
+      # Define initial state
+  import pinocchio as pin
+  # TODO: put in ocp maker
+  pinRefFrame = pin.LOCAL
+  ov = pin.getFrameVelocity(robot.model, robot.data, id_endeff, pin.WORLD).linear
+  of0 = -Kp*(oMf.translation- oPc) - Kv*ov
+  oRf = oMf.rotation
+  lf0 = oRf.T @ of0
+  fext0 = [pin.Force.Zero() for _ in range(robot.model.njoints)]
+  fext0[robot.model.frames[id_endeff].parent] = robot.model.frames[id_endeff].placement.act(pin.Force(lf0, np.zeros(3)))
+  ddp = OptimalControlProblemSoftContact(robot, config).initialize(x0,    
+      id_endeff, Kp, Kv, oPc, pinRefFrame, callbacks=True)
+  # Warmstart and solve
+  xs_init = [x0 for i in range(config['N_h']+1)]
+  us_init = [pin_utils.get_tau(q0, v0, np.zeros(nq), fext0, robot.model, np.zeros(nq)) for i in range(config['N_h'])] 
+  ddp.solve(xs_init, us_init, maxiter=config['maxiter'], isFeasible=False)
+
+  frame_of_interest = config['frame_of_interest']
+  if(PLOT_INIT):
+    ddp_handler = DDPDataHanlderSoftContact(ddp)
+    ddp_data = ddp_handler.extract_data(ee_frame_name=frame_of_interest, ct_frame_name=frame_of_interest)
+    # Extract soft force 
+    # TODO: put in data handlers
+    xs = np.array(ddp_data['xs'])
+    ps = pin_utils.get_p_(xs[:,:nq], robot.model, id_endeff)
+    vs = pin_utils.get_v_(xs[:,:nq], xs[:,nq:], robot.model, id_endeff, ref=pin.WORLD)
+    # Force in WORLD aligned frame
+    fs_lin = np.array([robot.data.oMf[id_endeff].rotation @ (-Kp*(ps[i,:] - oPc) - Kv*vs[i,:]) for i in range(config['N_h'])])
+    fs_ang = np.zeros((config['N_h'], 3))
+    ddp_data['fs'] = np.hstack([fs_lin, fs_ang])
+    ddp_data['force_ref'] = [np.zeros(6) for i in range(config['N_h']) ]
+    _, _ = ddp_handler.plot_ddp_results(ddp_data, markers=['.'], SHOW=True)
+
+  # # # # # # # # # # #
+  ### INIT MPC SIMU ###
+  # # # # # # # # # # #
+  sim_data = MPCDataHandlerSoftContact(config, robot)
+  sim_data.init_sim_data(x0)
+    # Replan & control counters
+  nb_plan = 0
+  nb_ctrl = 0
+  # Additional simulation blocks 
+  communicationModel = mpc_utils.CommunicationModel(config)
+  actuationModel     = mpc_utils.ActuationModel(config, nu=nu)
+  sensingModel       = mpc_utils.SensorModel(config)
+
+  # # # # # # # # # # # #
+  ### SIMULATION LOOP ###
+  # # # # # # # # # # # #
+
+  # SIMULATE
+  for i in range(sim_data.N_simu): 
+
+      if(i%config['log_rate']==0 and config['LOG']): 
+        print('')
+        logger.info("SIMU step "+str(i)+"/"+str(sim_data.N_simu))
+        print('')
+
+    # Solve OCP if we are in a planning cycle (MPC/planning frequency)
+      if(i%int(sim_data.simu_freq/sim_data.plan_freq) == 0):
+          # Reset x0 to measured state + warm-start solution
+          ddp.problem.x0 = sim_data.state_mea_SIMU[i, :]
+          xs_init = list(ddp.xs[1:]) + [ddp.xs[-1]]
+          xs_init[0] = sim_data.state_mea_SIMU[i, :]
+          us_init = list(ddp.us[1:]) + [ddp.us[-1]] 
+          # Solve OCP 
+          ddp.solve(xs_init, us_init, maxiter=config['maxiter'], isFeasible=False)
+          # Record MPC predictions, cost references and solver data 
+          sim_data.record_predictions(nb_plan, ddp)
+          sim_data.record_cost_references(nb_plan, ddp)
+          sim_data.record_solver_data(nb_plan, ddp) 
+          # Model communication delay between computer & robot (buffered OCP solution)
+          communicationModel.step(sim_data.x_pred, sim_data.u_curr)
+          # Record interpolated desired state, control and force at MPC frequency
+          sim_data.record_plan_cycle_desired(nb_plan)
+          # Increment planning counter
+          nb_plan += 1
+
+
+    # If we are in a control cycle select reference torque to send to the actuator (motor driver input frequency)
+      if(i%int(sim_data.simu_freq/sim_data.ctrl_freq) == 0):   
+          # Record interpolated desired state, control and force at CTRL frequency
+          sim_data.record_ctrl_cycle_desired(nb_ctrl)     
+          # Increment control counter
+          nb_ctrl += 1
+          
+          
+    # Simulate actuation/sensing and step simulator (physics simulation frequency)
+      # Record interpolated desired state, control and force at SIM frequency
+      sim_data.record_simu_cycle_desired(i)
+      # Actuation model ( tau_ref_SIMU ==> tau_mea_SIMU ) 
+      tau_mea_SIMU = actuationModel.step(i, sim_data.u_ref_SIMU, sim_data.ctrl_des_SIMU) 
+      # RICCATI GAINS TO INTERPOLATE
+      if(config['RICCATI']):
+        tau_mea_SIMU += ddp.K[0].dot(ddp.problem.x0 - sim_data.state_mea_SIMU[i,:])
+      #  Send output of actuation torque to the RBD simulator 
+      robot_simulator.send_joint_command(tau_mea_SIMU)
+      env.step()
+      # Measure new state from simulation 
+      q_mea_SIMU, v_mea_SIMU = robot_simulator.get_state()
+      # Update pinocchio model
+      robot_simulator.forward_robot(q_mea_SIMU, v_mea_SIMU)
+      f_mea_SIMU = simulator_utils.get_contact_wrench(robot_simulator, id_endeff, pinRefFrame)
+      if(i%50==0): 
+        logger.info("f_mea = "+str(f_mea_SIMU))
+      # Record data (unnoised)
+      x_mea_SIMU = np.concatenate([q_mea_SIMU, v_mea_SIMU]).T 
+      sim_data.state_mea_no_noise_SIMU[i+1, :] = x_mea_SIMU
+      # Sensor model ( simulation state ==> noised / filtered state )
+      sim_data.state_mea_SIMU[i+1, :] = sensingModel.step(i, x_mea_SIMU, sim_data.state_mea_SIMU)
+      sim_data.force_mea_SIMU[i, :] = f_mea_SIMU
+
+  # # # # # # # # # # #
+  # PLOT SIM RESULTS  #
+  # # # # # # # # # # #
+  save_dir = '/home/skleff/force-feedback/data'
+  save_name = config_name+'_'+simulator+'_'+\
+                          '_BIAS='+str(config['SCALE_TORQUES'])+\
+                          '_NOISE='+str(config['NOISE_STATE'] or config['NOISE_TORQUES'])+\
+                          '_DELAY='+str(config['DELAY_OCP'] or config['DELAY_SIM'])+\
+                          '_Fp='+str(sim_data.plan_freq/1000)+'_Fc='+str(sim_data.ctrl_freq/1000)+'_Fs'+str(sim_data.simu_freq/1000)
+  # Extract plot data from sim data
+  plot_data = sim_data.extract_data(frame_of_interest=frame_of_interest)
+  # Plot results
+  sim_data.plot_mpc_results(plot_data, which_plots=sim_data.WHICH_PLOTS,
+                                      PLOT_PREDICTIONS=True, 
+                                      pred_plot_sampling=int(sim_data.plan_freq/10),
+                                      SAVE=True,
+                                      SAVE_DIR=save_dir,
+                                      SAVE_NAME=save_name,
+                                      AUTOSCALE=True)
+  # Save optionally
+  if(config['SAVE_DATA']):
+    sim_data.save_data(sim_data, save_name=save_name, save_dir=save_dir)
+
+
+
+
+
+if __name__=='__main__':
+    args = misc_utils.parse_MPC_script(sys.argv[1:])
+    main(args.robot_name, args.simulator, args.PLOT_INIT)
