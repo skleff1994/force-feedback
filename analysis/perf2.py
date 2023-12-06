282a58c08b8a41d9ff1a0f3b85dd8e46fdabf1e1
Sebastien Kleff <sk8001@nyu.edu>
2023-12-06 16:21:53 -0500

    generated errors + std

--- analysis/perf2.py
+++ analysis/perf2.py
@@ -37,13 +37,13 @@ for n_seed in range(N_SEEDS):
     
     for n_exp in range(N_EXP):
         
-        torque_ratio_classical[n_seed, n_exp] = data_1['torque_errors'][0][n_seed, n_exp] / data_2['torque_errors'][0][n_seed, n_exp]
-        torque_ratio_lpf[n_seed, n_exp]       = data_1['torque_errors'][1][n_seed, n_exp] / data_2['torque_errors'][1][n_seed, n_exp]
-        torque_ratio_soft[n_seed, n_exp]      = data_1['torque_errors'][2][n_seed, n_exp] / data_2['torque_errors'][2][n_seed, n_exp]
+        torque_ratio_classical[n_seed, n_exp] = data_2['torque_errors'][0][n_seed, n_exp] - data_1['torque_errors'][0][n_seed, n_exp]
+        torque_ratio_lpf[n_seed, n_exp]       = data_2['torque_errors'][1][n_seed, n_exp] - data_1['torque_errors'][1][n_seed, n_exp]
+        torque_ratio_soft[n_seed, n_exp]      = data_2['torque_errors'][2][n_seed, n_exp] - data_1['torque_errors'][2][n_seed, n_exp]
         
-        force_ratio_classical[n_seed, n_exp] = data_1['force_errors_AVG'][0][n_seed, n_exp] / data_2['force_errors_AVG'][0][n_seed, n_exp]
-        force_ratio_lpf[n_seed, n_exp]       = data_1['force_errors_AVG'][1][n_seed, n_exp] / data_2['force_errors_AVG'][1][n_seed, n_exp]
-        force_ratio_soft[n_seed, n_exp]      = data_1['force_errors_AVG'][2][n_seed, n_exp] / data_2['force_errors_AVG'][2][n_seed, n_exp]
+        force_ratio_classical[n_seed, n_exp] = data_2['force_errors_AVG'][0][n_seed, n_exp] - data_1['force_errors_AVG'][0][n_seed, n_exp]
+        force_ratio_lpf[n_seed, n_exp]       = data_2['force_errors_AVG'][1][n_seed, n_exp] - data_1['force_errors_AVG'][1][n_seed, n_exp]
+        force_ratio_soft[n_seed, n_exp]      = data_2['force_errors_AVG'][2][n_seed, n_exp] - data_1['force_errors_AVG'][2][n_seed, n_exp]
 
 torque_ratio_classical_AVG = np.sum(torque_ratio_classical, axis=0) / N_SEEDS
 torque_ratio_lpf_AVG       = np.sum(torque_ratio_lpf, axis=0) / N_SEEDS
@@ -53,42 +53,104 @@ force_ratio_classical_AVG = np.sum(force_ratio_classical, axis=0) / N_SEEDS
 force_ratio_lpf_AVG       = np.sum(force_ratio_lpf, axis=0) / N_SEEDS
 force_ratio_soft_AVG      = np.sum(force_ratio_soft, axis=0) / N_SEEDS
 
-fig0, ax0 = plt.subplots(1, 1, figsize=(19.2,10.8)) # Torque errors
+# Force & position aboslute perf 1 (with torque control)
+print("--------")
+print("--------")
+print("[WITH TORQUE CONTROL] Position MAE (classical) = ", np.mean(data_1['position_errors'][0]), u"\u00B1", np.std(data_1['position_errors'][0]))
+print("[WITH TORQUE CONTROL] Position MAE (LPF)       = ", np.mean(data_1['position_errors'][1]), u"\u00B1", np.std(data_1['position_errors'][0]))
+print("[WITH TORQUE CONTROL] Position MAE (soft)      = ", np.mean(data_1['position_errors'][2]), u"\u00B1", np.std(data_1['position_errors'][0]))
+print("----")
+print("[WITH TORQUE CONTROL] Force MAE (classical) = ", np.mean(data_1['force_errors_AVG'][0]), u"\u00B1", np.std(data_1['force_errors_AVG'][0]))
+print("[WITH TORQUE CONTROL] Force MAE (LPF)       = ", np.mean(data_1['force_errors_AVG'][1]), u"\u00B1", np.std(data_1['force_errors_AVG'][1]))
+print("[WITH TORQUE CONTROL] Force MAE (soft)      = ", np.mean(data_1['force_errors_AVG'][2]), u"\u00B1", np.std(data_1['force_errors_AVG'][2]))
+print("----")
+print("[WITH TORQUE CONTROL] Force Max. (classical) = ", np.mean(data_1['force_errors_MAX'][0]), u"\u00B1", np.std(data_1['force_errors_MAX'][0]))
+print("[WITH TORQUE CONTROL] Force Max. (LPF)       = ", np.mean(data_1['force_errors_MAX'][1]), u"\u00B1", np.std(data_1['force_errors_MAX'][1]))
+print("[WITH TORQUE CONTROL] Force Max. (soft)      = ", np.mean(data_1['force_errors_MAX'][2]), u"\u00B1", np.std(data_1['force_errors_MAX'][2]))
+print("----")
+print("[WITH TORQUE CONTROL] Time not-in-contact (classical) = ", np.mean(data_1['cycles_not_in_contact'][0]), u"\u00B1", np.std(data_1['cycles_not_in_contact'][0]))
+print("[WITH TORQUE CONTROL] Time not-in-contact (LPF)       = ", np.mean(data_1['cycles_not_in_contact'][1]), u"\u00B1", np.std(data_1['cycles_not_in_contact'][1]))
+print("[WITH TORQUE CONTROL] Time not-in-contact (soft)      = ", np.mean(data_1['cycles_not_in_contact'][2]), u"\u00B1", np.std(data_1['cycles_not_in_contact'][2]))
+print("--------")
+print("--------")
 
-colors            = ['b', 'r', 'g']
-labels            = ['Classical MPC', 'Torque-feedback MPC', 'Force-feedback MPC']
-markers           = ['o', 's', 'D']
-torque_ratio_AVG  = [torque_ratio_classical_AVG, torque_ratio_lpf_AVG, torque_ratio_soft_AVG]
-force_ratio_AVG   = [force_ratio_classical_AVG, force_ratio_lpf_AVG, force_ratio_soft_AVG]
+# Force & position aboslute perf 2 (with torque control)
+print("--------")
+print("--------")
+print("[NO TORQUE CONTROL] Position MAE (classical) = ", np.mean(data_2['position_errors'][0]), u"\u00B1", np.std(data_2['position_errors'][0]))
+print("[NO TORQUE CONTROL] Position MAE (LPF)       = ", np.mean(data_2['position_errors'][1]), u"\u00B1", np.std(data_2['position_errors'][0]))
+print("[NO TORQUE CONTROL] Position MAE (soft)      = ", np.mean(data_2['position_errors'][2]), u"\u00B1", np.std(data_2['position_errors'][0]))
+print("----")
+print("[NO TORQUE CONTROL] Force MAE (classical) = ", np.mean(data_2['force_errors_AVG'][0]), u"\u00B1", np.std(data_2['force_errors_AVG'][0]))
+print("[NO TORQUE CONTROL] Force MAE (LPF)       = ", np.mean(data_2['force_errors_AVG'][1]), u"\u00B1", np.std(data_2['force_errors_AVG'][1]))
+print("[NO TORQUE CONTROL] Force MAE (soft)      = ", np.mean(data_2['force_errors_AVG'][2]), u"\u00B1", np.std(data_2['force_errors_AVG'][2]))
+print("----")
+print("[NO TORQUE CONTROL] Force Max. (classical) = ", np.mean(data_2['force_errors_MAX'][0]), u"\u00B1", np.std(data_2['force_errors_MAX'][0]))
+print("[NO TORQUE CONTROL] Force Max. (LPF)       = ", np.mean(data_2['force_errors_MAX'][1]), u"\u00B1", np.std(data_2['force_errors_MAX'][1]))
+print("[NO TORQUE CONTROL] Force Max. (soft)      = ", np.mean(data_2['force_errors_MAX'][2]), u"\u00B1", np.std(data_2['force_errors_MAX'][2]))
+print("----")
+print("[NO TORQUE CONTROL] Time not-in-contact (classical) = ", np.mean(data_2['cycles_not_in_contact'][0]), u"\u00B1", np.std(data_2['cycles_not_in_contact'][0]))
+print("[NO TORQUE CONTROL] Time not-in-contact (LPF)       = ", np.mean(data_2['cycles_not_in_contact'][1]), u"\u00B1", np.std(data_2['cycles_not_in_contact'][1]))
+print("[NO TORQUE CONTROL] Time not-in-contact (soft)      = ", np.mean(data_2['cycles_not_in_contact'][2]), u"\u00B1", np.std(data_2['cycles_not_in_contact'][2]))
+print("--------")
+print("--------")
+# # Force & position aboslute perf 2 (without torque control)
+# print("Classical torque perf change = ", np.mean(torque_ratio_classical_AVG))
+# print("LPF torque perf change = ",  np.mean(torque_ratio_lpf_AVG))
+# print("Soft torque perf change = ",  np.mean(torque_ratio_soft_AVG))
+# print("----")
 
-torque_ratios    = [torque_ratio_classical, torque_ratio_lpf, torque_ratio_soft]
-force_ratios     = [force_ratio_classical, force_ratio_lpf, force_ratio_soft]
+# # aboslute perf
+# print("Classical force perf change = ", np.mean(force_ratio_classical_AVG))
+# print("LPF force perf change = ",  np.mean(force_ratio_lpf_AVG))
+# print("Soft force perf change = ",  np.mean(force_ratio_soft_AVG))
+# print("----")
 
-for i in range(3):
-    # Plot average lines
-    if(n_seed == N_SEEDS-1 and n_exp == N_EXP-1):
-        lab = labels[i]
-    else:
-        lab = None
-    ax0.plot(TILT_ANGLES_DEG, force_ratio_AVG[i], color=colors[i], linestyle='-', linewidth=3, label=lab)
-    # ax0.plot(TILT_ANGLES_DEG, force_ratio_AVG[i], color=colors[i], linestyle='-', linewidth=3, label=lab)
+# print("Classical torque perf change = ", np.mean(torque_ratio_classical_AVG))
+# print("LPF torque perf change = ",  np.mean(torque_ratio_lpf_AVG))
+# print("Soft torque perf change = ",  np.mean(torque_ratio_soft_AVG))
+# print("----")
 
-    # For each experiment plot perf as marker 
-    for n_exp in range(N_EXP): 
-        ax0.plot(float(TILT_ANGLES_DEG[n_exp]), force_ratio_AVG[i][n_exp], 
-                                                marker=markers[i], markerfacecolor=colors[i], 
-                                                markersize=18, markeredgecolor='k')
-        # Plot each seed exp with  transparent marker
-        for n_seed in range(N_SEEDS):
-            ax0.plot(TILT_ANGLES_DEG[n_exp], force_ratios[i][n_seed,n_exp], marker=markers[i], markerfacecolor=colors[i], markersize=16, alpha=0.3)
-            # ax0.plot(TILT_ANGLES_DEG[n_exp], torque_ratios[i][n_seed,n_exp], marker=markers[i], markerfacecolor=colors[i], markersize=16, alpha=0.3)
+# print("Classical perf ratio = ", np.mean(force_ratio_classical_AVG)/np.mean(torque_ratio_classical_AVG))
+# print("LPF perf ratio = ",  np.mean(force_ratio_lpf_AVG)/np.mean(torque_ratio_lpf_AVG))
+# print("Soft perf ratio = ",  np.mean(force_ratio_soft_AVG)/np.mean(torque_ratio_soft_AVG))
+
+# fig0, ax0 = plt.subplots(1, 1, figsize=(19.2,10.8)) # Torque errors
+
+# colors            = ['b', 'r', 'g']
+# labels            = ['Classical MPC', 'Torque-feedback MPC', 'Force-feedback MPC']
+# markers           = ['o', 's', 'D']
+# torque_ratio_AVG  = [torque_ratio_classical_AVG, torque_ratio_lpf_AVG, torque_ratio_soft_AVG]
+# force_ratio_AVG   = [force_ratio_classical_AVG, force_ratio_lpf_AVG, force_ratio_soft_AVG]
+
+# torque_ratios    = [torque_ratio_classical, torque_ratio_lpf, torque_ratio_soft]
+# force_ratios     = [force_ratio_classical, force_ratio_lpf, force_ratio_soft]
+
+# for i in range(3):
+#     # Plot average lines
+#     if(n_seed == N_SEEDS-1 and n_exp == N_EXP-1):
+#         lab = labels[i]
+#     else:
+#         lab = None
+#     ax0.plot(TILT_ANGLES_DEG, force_ratio_AVG[i]/torque_ratio_AVG[i], color=colors[i], linestyle='-', linewidth=3, label=lab)
+#     # ax0.plot(TILT_ANGLES_DEG, force_ratio_AVG[i], color=colors[i], linestyle='-', linewidth=3, label=lab)
+
+#     # For each experiment plot perf as marker 
+#     for n_exp in range(N_EXP): 
+#         ax0.plot(float(TILT_ANGLES_DEG[n_exp]), force_ratio_AVG[i][n_exp]/torque_ratio_AVG[i][n_exp], 
+#                                                 marker=markers[i], markerfacecolor=colors[i], 
+#                                                 markersize=18, markeredgecolor='k')
+#         # Plot each seed exp with  transparent marker
+#         for n_seed in range(N_SEEDS):
+#             ax0.plot(TILT_ANGLES_DEG[n_exp], force_ratios[i][n_seed,n_exp]/torque_ratios[i][n_seed,n_exp], marker=markers[i], markerfacecolor=colors[i], markersize=16, alpha=0.3)
+#             # ax0.plot(TILT_ANGLES_DEG[n_exp], torque_ratios[i][n_seed,n_exp], marker=markers[i], markerfacecolor=colors[i], markersize=16, alpha=0.3)
             
-# Set axis and stuff
-ax0.set_ylabel('$|| \tau^{mea} - \tau^{des} ||$  (N)', fontsize=26)
-ax0.yaxis.set_major_locator(plt.MaxNLocator(2))
-ax0.yaxis.set_major_formatter(plt.FormatStrFormatter('%.1e'))
-ax0.grid(True) 
-ax0.tick_params(axis = 'y', labelsize=22)
-ax0.set_xlabel('Angle (deg)', fontsize=26)
-ax0.tick_params(axis = 'x', labelsize = 22)
-plt.show()
+# # Set axis and stuff
+# ax0.set_ylabel('Performance ratio', fontsize=26)
+# ax0.yaxis.set_major_locator(plt.MaxNLocator(2))
+# ax0.yaxis.set_major_formatter(plt.FormatStrFormatter('%.1e'))
+# ax0.grid(True) 
+# ax0.tick_params(axis = 'y', labelsize=22)
+# ax0.set_xlabel('Angle (deg)', fontsize=26)
+# ax0.tick_params(axis = 'x', labelsize = 22)
+# plt.show()
